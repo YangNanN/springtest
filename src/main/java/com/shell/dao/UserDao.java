8b6a231f70a478303a28c71806998ab5c522eda1
shellqiqi <shellqiqi@outlook.com>
2017-09-03 01:02:37 +0800

    Add spring config

--- src/main/java/com/shell/dao/UserDao.java
+++ src/main/java/com/shell/dao/UserDao.java
@@ -6,7 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.jdbc.core.JdbcTemplate;
